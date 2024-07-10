8e4f0ee3ed76f589ffd6b52d39eb68351a0b9878
Jaehyuk0922 <dhwogur0@gmail.com>
2024-07-10 21:42:35 +0900

    Update 1. 문자열 뒤집기.java

--- study/ DAY6/오재혁/1. 문자열 뒤집기.java
+++ study/ DAY6/오재혁/1. 문자열 뒤집기.java
@@ -1,1 +1,11 @@
-
+class Solution {
+   public String solution(String my_string) {
+        char[] change = my_string.toCharArray();
+        char[] reverse_change = new char[my_string.length()];
+        for(int i = 0 ; i < my_string.length() ; i++ ){
+            reverse_change[i] = change[my_string.length()-i-1];
+        }
+       
+        return String.valueOf(reverse_change);
+    }
+}
