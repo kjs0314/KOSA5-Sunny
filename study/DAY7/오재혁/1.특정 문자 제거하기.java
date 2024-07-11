6ffc016195398378ce39d5339616e024a034b218
Jaehyuk0922 <dhwogur0@gmail.com>
2024-07-11 13:28:48 +0000

    Update 1.특정 문자 제거하기.java

--- study/DAY7/오재혁/1.특정 문자 제거하기.java
+++ study/DAY7/오재혁/1.특정 문자 제거하기.java
@@ -1,1 +1,12 @@
-
+class Solution {
+       public String solution(String my_string, String letter) {
+        String answer = "";
+        char[] arr = my_string.toCharArray();
+        for( int i=0; i<my_string.length(); i++){
+            if(arr[i] != letter.charAt(0)){
+                answer +=  arr[i];
+            }
+        }
+        return answer;
+    }
+}
